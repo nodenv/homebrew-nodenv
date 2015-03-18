25258c757e0e633d8d880d0d4b598d836025f253
Josh Hagins <hagins.josh@gmail.com>
2015-03-18 18:11:38 -0400

    Add formula for OiNutter/nodenv

---
+++ nodenv.rb
@@ -0,0 +1,16 @@
+class Nodenv < Formula
+  homepage "https://github.com/OiNutter/nodenv"
+  head "https://github.com/OiNutter/nodenv.git"
+  url "https://github.com/OiNutter/nodenv/archive/v0.2.0.tar.gz"
+  sha1 "ce66e6a546ad92b166c4133796df11cd9fbbbd5f"
+
+  def install
+    prefix.install "bin", "libexec"
+  end
+
+  def caveats; <<-EOS.undent
+    To enable shims and autocompletion add to your profile:
+      if which nodenv > /dev/null; then eval "$(nodenv init -)"; fi
+    EOS
+  end
+end
