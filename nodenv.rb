3e2d66735e871921edf28669db8de79b1bf07ba6
Josh Hagins <hagins.josh@gmail.com>
2015-03-18 20:31:20 -0400

    Fix NODENV_ROOT caveat

--- nodenv.rb
+++ nodenv.rb
@@ -5,6 +5,7 @@ class Nodenv < Formula
   sha256 "3e0c8f81407741ac4b8ca4a07e03269e13ff00e15057f8fbdf0a195068043b60"
 
   def install
+    inreplace "libexec/rbenv", "/usr/local", HOMEBREW_PREFIX
     prefix.install "bin", "libexec"
   end
 
@@ -13,7 +14,7 @@ class Nodenv < Formula
       if which nodenv > /dev/null; then eval "$(nodenv init -)"; fi
 
     To use Homebrew's directories rather than ~/.nodenv add to your profile:
-      export NODENV_ROOT=#{opt_prefix}/nodenv
+      export NODENV_ROOT=#{opt_prefix}
     EOS
   end
 
