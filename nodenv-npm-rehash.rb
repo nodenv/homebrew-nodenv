15880a5e4585e494f616c17ba3a791847ee3dee3
Josh Hagins <hagins.josh@gmail.com>
2015-03-31 10:57:16 -0400

    Fix sha256 hash

--- nodenv-npm-rehash.rb
+++ nodenv-npm-rehash.rb
@@ -2,7 +2,7 @@ class NodenvNpmRehash < Formula
   homepage "https://github.com/jawshooah/nodenv-npm-rehash"
   head "https://github.com/jawshooah/nodenv-npm-rehash.git"
   url "https://github.com/jawshooah/nodenv-npm-rehash/archive/0.1.0.tar.gz"
-  sha256 "bfccf01590f5ba76bb782c723960ad8a832775503ae6338e84dbc45f96a91ac1"
+  sha256 "df7d20a09e9ed17ca98ee87b70dd371a7beed12eb2c099d110458a72d2e816d1"
 
   depends_on "jawshooah/nodenv/nodenv"
 
