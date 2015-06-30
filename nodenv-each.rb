959593b623ca080c2cf227a6b66e2b7a14e33113
Josh Hagins <hagins.josh@gmail.com>
2015-06-29 23:50:51 -0400

    nodenv-each v1.0.0

--- nodenv-each.rb
+++ nodenv-each.rb
@@ -1,5 +1,7 @@
 class NodenvEach < Formula
   homepage "https://github.com/jasonkarns/nodenv-each"
+  url "https://github.com/jasonkarns/nodenv-each/archive/v1.0.0.tar.gz"
+  sha256 "a6e32cfc029407d92dc9d193037a647247ed65ead9187ffaf953abacf8b6b94e"
   head "https://github.com/jasonkarns/nodenv-each.git"
 
   depends_on "jawshooah/nodenv/nodenv"
