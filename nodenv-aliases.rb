b0855cfaaf544239ee4d68c51cd41dcb0ceb5915
Josh Hagins <hagins.josh@gmail.com>
2015-06-29 23:52:52 -0400

    nodenv-aliases v1.0.0

--- nodenv-aliases.rb
+++ nodenv-aliases.rb
@@ -1,5 +1,7 @@
 class NodenvAliases < Formula
   homepage "https://github.com/jasonkarns/nodenv-aliases"
+  url "https://github.com/jasonkarns/nodenv-aliases/archive/v1.0.0.tar.gz"
+  sha256 "968d61ba55f2e6b0b137f2d7b8765beeabb4d789d73d5c88a9c4dd559721eb25"
   head "https://github.com/jasonkarns/nodenv-aliases.git"
 
   depends_on "jawshooah/nodenv/nodenv"
