365930ab63f406f5891524c95d45de58dde8d8e9
Josh Hagins <hagins.josh@gmail.com>
2015-06-29 23:57:26 -0400

    nodenv-package-rehash v1.0.2

--- nodenv-package-rehash.rb
+++ nodenv-package-rehash.rb
@@ -1,8 +1,8 @@
 class NodenvPackageRehash < Formula
   homepage "https://github.com/jasonkarns/nodenv-package-rehash"
   head "https://github.com/jasonkarns/nodenv-package-rehash.git"
-  url "https://github.com/jasonkarns/nodenv-package-rehash/archive/v1.0.0.tar.gz"
-  sha256 "590eba5ddaf4250912a748a09ff76b120d57eb3cb81532156c7732a0eb2fc580"
+  url "https://github.com/jasonkarns/nodenv-package-rehash/archive/v1.0.2.tar.gz"
+  sha256 "7811b5fc85e472c93fe7f83f72a7a34c82f31d2fee671ab6e6ba8b44ee2071b0"
 
   depends_on "jawshooah/nodenv/nodenv"
 
