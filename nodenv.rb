5f7a0a8de3fb1b16fcb7bae894d582e0bc64b49d
Josh Hagins <hagins.josh@gmail.com>
2015-07-21 10:08:46 -0400

    nodenv: Install only bin, libexec, completions
    Closes #3

--- nodenv.rb
+++ nodenv.rb
@@ -3,10 +3,11 @@ class Nodenv < Formula
   head "https://github.com/OiNutter/nodenv.git"
   url "https://github.com/OiNutter/nodenv/archive/v0.3.0.tar.gz"
   sha256 "f8486ce3a6bef589ec81ecd3db7e8154e52a126a58a2755a450c4a2300ae053f"
+  revision 1
 
   def install
     inreplace "libexec/nodenv", "/usr/local", HOMEBREW_PREFIX
-    prefix.install Dir["*"]
+    prefix.install "bin", "libexec", "completions"
   end
 
   def caveats; <<-EOS.undent
