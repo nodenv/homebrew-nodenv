3cf171d96a76895c93007dd93c495c20c0e84fc4
Adam Hull <adam@hmlad.com>
2015-11-07 19:52:06 -0800

    Fix package.json homebrew formula

--- nodenv-package-json-engine.rb
+++ nodenv-package-json-engine.rb
@@ -1,17 +1,17 @@
-class NodenvPackageJSONEngine < Formula
+class NodenvPackageJsonEngine < Formula
   desc "Pick a node version from package.json engines"
   homepage "https://github.com/hurrymaplelad/nodenv-package-json-engine"
   url "https://github.com/hurrymaplelad/nodenv-package-json-engine/archive/v1.0.0.tar.gz"
   sha256 "85bbd854e4c377549ea45a25c878ae42ce81ff72a9991e11bffe0e012afb9282"
   head "https://github.com/hurrymaplelad/nodenv-package-json-engine.git"
 
-  depends_on "jawshooah/nodenv/nodenv"
+  depends_on "nodenv"
 
   def install
     prefix.install Dir["*"]
   end
 
   test do
-    assert_match /^package-json-engine$/, shell_output("nodenv hooks which")
+    assert_match /\bpackage-json-engine\b/, shell_output("nodenv hooks which")
   end
 end
