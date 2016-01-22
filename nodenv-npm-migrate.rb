518890497719dc55004aac1357bed0f98b6d8704
Jason Karns <jason@karns.name>
2016-01-22 09:12:14 -0500

    moved to nodenv org

--- nodenv-npm-migrate.rb
+++ nodenv-npm-migrate.rb
@@ -1,9 +1,9 @@
 class NodenvNpmMigrate < Formula
   desc "Migrate npm packages from one Node version to another"
-  homepage "https://github.com/jawshooah/nodenv-npm-migrate"
-  url "https://github.com/jawshooah/nodenv-npm-migrate/archive/0.1.0.tar.gz"
+  homepage "https://github.com/nodenv/nodenv-npm-migrate"
+  url "https://github.com/nodenv/nodenv-npm-migrate/archive/0.1.0.tar.gz"
   sha256 "4c35dae6a6ca79a8b00083989b4dc18d5cb0d8bb2651de48e3996ab85403e225"
-  head "https://github.com/jawshooah/nodenv-npm-migrate.git"
+  head "https://github.com/nodenv/nodenv-npm-migrate.git"
 
   depends_on "nodenv"
 
