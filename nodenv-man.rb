b0dcd2422924ce45a12ccd9e22f09e0db265cf56
Jason Karns <jason@karns.name>
2016-02-09 09:47:10 -0500

    update sha

--- nodenv-man.rb
+++ nodenv-man.rb
@@ -2,7 +2,7 @@ class NodenvMan < Formula
   desc "View man pages for currently active node version"
   homepage "https://github.com/nodenv/nodenv-man"
   url "https://github.com/nodenv/nodenv-man/archive/v1.0.0.tar.gz"
-  sha256 "ea5e4134b1bd0df535668a178e1eaa5dfddcb5dec0624b97e6b7ab9c2afbc149"
+  sha256 "495c63015645d8655d925bafc291aa38e2585788bb2dd7082a0447aed049ce98"
   head "https://github.com/nodenv/nodenv-man.git"
 
   depends_on "nodenv"
