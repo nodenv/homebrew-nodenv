22776006dbc591aba4331ece95e18764e8d94948
Jason Karns <jason@karns.name>
2017-01-25 11:14:13 -0500

    Points package-json-engine urls to nodenv org

--- nodenv-package-json-engine.rb
+++ nodenv-package-json-engine.rb
@@ -1,9 +1,9 @@
 class NodenvPackageJsonEngine < Formula
   desc "Pick a node version from package.json engines"
-  homepage "https://github.com/hurrymaplelad/nodenv-package-json-engine"
+  homepage "https://github.com/nodenv/nodenv-package-json-engine"
   url "https://github.com/nodenv/nodenv-package-json-engine/archive/v1.1.1.tar.gz"
   sha256 "6ac9423aa9ea2c604ba6b70a47cfa84b3c8a595572e8ff67d3afa3ba504491e2"
-  head "https://github.com/hurrymaplelad/nodenv-package-json-engine.git"
+  head "https://github.com/nodenv/nodenv-package-json-engine.git"
 
   depends_on "nodenv"
 
