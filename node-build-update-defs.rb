17d470dcfa078bba869ba3b12eaaaa6e64fbec65
Jason Karns <jason@karns.name>
2017-05-25 15:16:57 -0400

    node-build-update-defs 2.1.2

--- node-build-update-defs.rb
+++ node-build-update-defs.rb
@@ -1,8 +1,8 @@
 class NodeBuildUpdateDefs < Formula
   desc "Scrape build definitions from nodejs.org and iojs.org"
   homepage "https://github.com/nodenv/node-build-update-defs"
-  url "https://github.com/nodenv/node-build-update-defs/archive/v2.1.1.tar.gz"
-  sha256 "187205d07058accdbb5b0ab752e138a473b84e8e7b769bc792a5656dec1b3f3d"
+  url "https://github.com/nodenv/node-build-update-defs/archive/v2.1.2.tar.gz"
+  sha256 "00cc679a286d66a47cf75c2f150142d4907f25526e841d1048b98a2b118b8cab"
   head "https://github.com/nodenv/node-build-update-defs.git"
 
   depends_on "nodenv"
