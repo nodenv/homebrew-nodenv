6d82d963e577f544cd81f060fb390b57ca8efe2a
Adam Vigneaux <adam@adamvig.com>
2018-08-08 14:44:07 -0400

    nodenv-default-packages 0.2.1

--- nodenv-default-packages.rb
+++ nodenv-default-packages.rb
@@ -1,8 +1,8 @@
 class NodenvDefaultPackages < Formula
   desc "Auto-installs packages for Node installs"
   homepage "https://github.com/nodenv/nodenv-default-packages"
-  url "https://github.com/nodenv/nodenv-default-packages/archive/v0.2.0.tar.gz"
-  sha256 "18af005fd2504dba20cb39215dc3ae20571b29f42aaef421654d679cec553f9b"
+  url "https://github.com/6RiverSystems/nodenv-default-packages/archive/v0.2.1.tar.gz"
+  sha256 "c738875b345055c42ad99732c2a4bdba688a5bad5f524fa62db3174915b7d97e"
   head "https://github.com/nodenv/nodenv-default-packages.git"
 
   depends_on "nodenv"
