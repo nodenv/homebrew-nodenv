af203bbb84f2381c9ca750408b5dfe638c8eb6b1
Jason Karns <jason@karns.name>
2018-10-31 16:55:31 -0400

    nodenv-each 1.2.0

--- nodenv-each.rb
+++ nodenv-each.rb
@@ -1,8 +1,8 @@
 class NodenvEach < Formula
   desc "Run a command against all installed versions of Node"
   homepage "https://github.com/nodenv/nodenv-each"
-  url "https://github.com/nodenv/nodenv-each/archive/v1.1.0.tar.gz"
-  sha256 "b398dabc5569f084b64b1daa377c5fd7ae508a166c5583f9eace00a6aeadf029"
+  url "https://github.com/nodenv/nodenv-each/archive/v1.2.0.tar.gz"
+  sha256 "be88cdbd74103657097bd349f7804b67063fddbde81cef8864776a8f13e8f945"
   head "https://github.com/nodenv/nodenv-each.git"
 
   depends_on "nodenv"
