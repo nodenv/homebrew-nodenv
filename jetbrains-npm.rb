67ad771f3cb590fa750e2fefe874f113437230f4
Jason Karns <jason@karns.name>
2019-01-09 17:03:31 -0500

    Adds jetbrains-npm formula

---
+++ jetbrains-npm.rb
@@ -0,0 +1,24 @@
+class JetbrainsNpm < Formula
+  desc "Fix IntelliJ/WebStorm's npm integration under nodenv"
+  homepage "https://github.com/nodenv/jetbrains-npm"
+  url "https://github.com/nodenv/jetbrains-npm/archive/v1.0.0.tar.gz"
+  sha256 "4083ab47eed1e614d374f117722628f568de465f6cb0481b2ffc830c2c1c7afe"
+  head "https://github.com/nodenv/jetbrains-npm.git"
+
+  depends_on "nodenv"
+
+  keg_only "this formula is only used by IntelliJ/WebStorm"
+
+  def install
+    prefix.install "bin"
+  end
+
+  def caveats
+    <<~MSG
+      Configure IntelliJ/WebStorm's Package manager path as:
+        #{opt_prefix}
+      And ensure `NODENV_ROOT' is set in your JetBrains environment.
+        https://github.com/nodenv/jetbrains-npm#configuration
+    MSG
+  end
+end
