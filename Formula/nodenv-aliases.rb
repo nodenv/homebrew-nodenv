2d060795997c975d01042e794b6b2a448b055ec0
jasonkarns <jasonkarns@users.noreply.github.com>
2020-03-30 19:16:50 +0000

    nodenv-aliases 2.0.2

--- Formula/nodenv-aliases.rb
+++ Formula/nodenv-aliases.rb
@@ -1,8 +1,8 @@
 class NodenvAliases < Formula
   desc "Create aliases for nodenv Node versions"
   homepage "https://github.com/nodenv/nodenv-aliases"
-  url "https://github.com/nodenv/nodenv-aliases/archive/v2.0.1.tar.gz"
-  sha256 "829651b115898b2d04f075be489599679e4882325c2a1239129558afc9602934"
+  url "https://github.com/nodenv/nodenv-aliases/archive/v2.0.2.tar.gz"
+  sha256 "632193b462438c986c104edc95f5378c067e02f6f8d1a7be38d5017183ab0149"
   head "https://github.com/nodenv/nodenv-aliases.git"
 
   depends_on "nodenv"
