deb6aed32f79e48b1803e910bd4f288379945bf6
Jason Karns <jason.karns@gmail.com>
2025-08-29 15:46:51 -0400

    Update repo urls after nodenv-nvmrc was pulled into nodenv org

--- Formula/nodenv-nvmrc.rb
+++ Formula/nodenv-nvmrc.rb
@@ -1,9 +1,9 @@
 class NodenvNvmrc < Formula
   desc "Pick a node version from .nvmrc"
-  homepage "https://github.com/ouchxp/nodenv-nvmrc"
-  url "https://github.com/ouchxp/nodenv-nvmrc/archive/refs/tags/v1.0.6.tar.gz"
+  homepage "https://github.com/nodenv/nodenv-nvmrc"
+  url "https://github.com/nodenv/nodenv-nvmrc/archive/refs/tags/v1.0.6.tar.gz"
   sha256 "8381f5d30edd76251bd218db893823c2d0cc82e5b8a29bd03daa1afe6f02d66d"
-  head "https://github.com/ouchxp/nodenv-nvmrc.git"
+  head "https://github.com/nodenv/nodenv-nvmrc.git"
 
   depends_on "nodenv"
 
