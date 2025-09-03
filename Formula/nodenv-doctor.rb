6356034b00ee7517f4921a7ec2ae2e4888d33fb7
Jason Karns <jason.karns@gmail.com>
2025-09-03 15:47:35 -0400

    nodenv-doctor 2.0.1

---
+++ Formula/nodenv-doctor.rb
@@ -0,0 +1,18 @@
+class NodenvDoctor < Formula
+  desc "Installer and doctor scripts for nodenv"
+  homepage "https://github.com/nodenv/nodenv-installer"
+  url "https://github.com/nodenv/nodenv-installer/archive/refs/tags/v2.0.1.tar.gz"
+  sha256 "4385d861a76b235f48614d8ab8711f57103a8d708a55cc726753d4083ec7f5a1"
+  head "https://github.com/nodenv/nodenv-installer.git", branch: "main"
+  license "CC0-1.0"
+
+  depends_on "nodenv"
+
+  def install
+    bin.install "bin/nodenv-doctor"
+  end
+
+  test do
+    assert_match(/^doctor$/, shell_output("nodenv commands"))
+  end
+end
