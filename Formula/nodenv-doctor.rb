2993a5ca86d019d1cafbcafc7ac193cb7fbbf29d
Jason Karns <jason.karns@gmail.com>
2025-09-03 15:51:01 -0400

    Update nodenv-doctor description

--- Formula/nodenv-doctor.rb
+++ Formula/nodenv-doctor.rb
@@ -1,5 +1,5 @@
 class NodenvDoctor < Formula
-  desc "Installer and doctor scripts for nodenv"
+  desc "Healthcheck status and debugging subcommand for nodenv"
   homepage "https://github.com/nodenv/nodenv-installer"
   url "https://github.com/nodenv/nodenv-installer/archive/refs/tags/v2.0.1.tar.gz"
   sha256 "4385d861a76b235f48614d8ab8711f57103a8d708a55cc726753d4083ec7f5a1"
